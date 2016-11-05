2863c518d0605024ccb40b852a142c0b52a333ef
Ryan <Ryan_Patterson@yahoo.com>
2016-11-05 11:07:39 -0500

    5.1 Add routes and redirect to app

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
-  root 'static_pages#landing_page'
+  get 'static_pages/landing_page'
+
+  root 'static_pages#index'
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
