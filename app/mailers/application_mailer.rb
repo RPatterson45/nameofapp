eaf566ebb27316c7051fd333e057f5fff979142a
Ryan <Ryan_Patterson@yahoo.com>
2016-11-08 13:27:51 -0600

    5.3 Add sendgrid actionmailer and style email template

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'Ryan_Patterson@yahoo.com'
   layout 'mailer'
 end
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ApplicationMailer
+	default from: "Ryan_Patterson@yahoo.com"
+
+  def contact_form(email, name, message)
+  @message = message
+    mail(:from => email,
+        :to => 'Ryan_Patterson@yahoo.com',
+        :subject => "A new contact message from #{@name}")
+  end
+end
