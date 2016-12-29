1bdb5df4fad2fae460c9c1bcdab0821be9062f25
Ryan <Ryan_Patterson@yahoo.com>
2016-12-29 16:38:15 -0600

    Fix users_controllers_spec and ratyrate star images

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,31 +3,33 @@ require 'rails_helper'
 describe UsersController, type: :controller do
   #let(:user_1) { User.create!(email: 'Leona_Patterson@example.com', password: 'Leona45') }
   #let(:user_2) { User.create!(email: 'Brigid@test.com', password: 'Brigid10') }
-  user_1 = FactoryGirl.create(:user)
-  user_2 = FactoryGirl.create(:user)
+  before do
+    @user_1 = FactoryGirl.create(:user)
+    @user_2 = FactoryGirl.create(:user)
+  end
 
   describe 'GET #show' do
     context 'User is logged in' do
       before do
-        sign_in user_1
+        sign_in @user_1
       end
 
       it 'Loads correct user details' do
-        get :show, id: user_1.id
+        get :show, id: @user_1.id
         expect(response).to be_success
         expect(response).to have_http_status(200)
-        expect(assigns(:user)).to eq user_1
+        expect(assigns(:user)).to eq @user_1
       end
 
       it 'redirected to root_path' do
-        get :show, id: user_2.id
+        get :show, id: @user_2.id
         expect(response).to redirect_to(root_path) 
       end
     end
 
     context 'No user is logged in' do
       it 'redirects to login' do
-        get :show, id: user_1.id
+        get :show, id: @user_1.id
         expect(response).to redirect_to(root_path)
       end  
     end
